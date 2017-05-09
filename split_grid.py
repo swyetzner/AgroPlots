52fec85908975dacbe49b95d39515961ffcd9efa
Sofia Wyetzner <sofia.wyetzner@gmail.com>
2017-05-08 22:58:33 -0500

    swapping works

--- split_grid.py
+++ split_grid.py
@@ -31,14 +31,6 @@ class PixelBlock:
 			self.pixels = rgba
 
 
-# class for system of agricultural plots
-class BlockSystem:
-	def __init__(self, width, height):
-		self.blocks = []
-		self.width = width
-		self.height = height
-
-
 # returns color difference between two rgba arrays
 def colorDiff(rgba1, rgba2):
 	totalDiff = 0
@@ -46,12 +38,11 @@ def colorDiff(rgba1, rgba2):
 		totalDiff += rgba1[i] - rgba2[i]
 	return totalDiff
 
+def importImage(name):
+	return Image.open(name)
+
 # tilePlots()
 # General: splits image into blocks of alike pixels
-# Details: finds one plot and uses the size to assume dimensions for 
-# the opthers because not every plot might have a large gradient with 
-# neighboring plot
-# Args: image, array of PixelBlocks
 #
 def tile(im, wp, hp, plots):
 	plotWidth = im.width/wp
@@ -75,22 +66,35 @@ def showTiles(im, wp, hp, plots):
 					im.putpixel((ii, jj), white)
 
 
+# swaps (x1,y1) with (x2,y2)
+def swapTiles(im, wp, hp, coord1, coord2, plots):
+	# convert to 1d
+	p = coord1[1]*wp + coord1[0]
+	q = coord2[1]*wp + coord2[0]
+	for i in xrange(plots[p].width):
+		for j in xrange(plots[p].height):
+			color1 = im.getpixel((i+plots[p].ul[0],j+plots[p].ul[1]))
+			color2 = im.getpixel((i+plots[q].ul[0],j+plots[q].ul[1]))
+			im.putpixel((i+plots[p].ul[0],j+plots[p].ul[1]),color2)
+			im.putpixel((i+plots[q].ul[0],j+plots[q].ul[1]),color1)
+
+
 # ------------- MAIN -----------------
-image_name = sys.argv[1]
-im = Image.open(image_name)
+#image_name = sys.argv[1]
+#im = Image.open(image_name)
 
 # get size of image
-w = im.width
-h = im.height
+#w = im.width
+#h = im.height
 # agricultural plots array
 agriPlots = []
 # x by x plots
-wplots = int(sys.argv[2])
-hplots = int(sys.argv[3])
+#wplots = int(sys.argv[2])
+#hplots = int(sys.argv[3])
 
-tile(im, wplots, hplots, agriPlots)
-showTiles(im, wplots, hplots, agriPlots)
-im.show()
+#tile(im, wplots, hplots, agriPlots)
+#showTiles(im, wplots, hplots, agriPlots)
+#im.show()
 
 
 
